6e453fcb6fcec8f863f759f043db82b970173821
AgnaldoCordeiro <agske13@gmail.com>
2022-11-19 11:55:22 -0300

    :hammer: contruindo cards

---
+++ src/components/CardImgReverse/index.tsx
@@ -0,0 +1,27 @@
+import styles from "./styles.module.scss";
+
+interface CardProps {
+  title: string;
+  img: string;
+  subTexto: string;
+}
+
+export function CardImgReverse({ title, img, subTexto }: CardProps) {
+  return (
+    <div className={styles.subContainer}>
+      <div className={styles.card}>
+        <div className={styles.tituloCard}>
+          <h6>{title}</h6>
+          <p>{subTexto}</p>
+          <a>Saiba mais <i> <svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0.295018 2.33114L4.87502 6.92114L0.295018 11.5111L1.70502 12.9211L7.70502 6.92114L1.70502 0.921143L0.295018 2.33114Z" fill="#980138" />
+          </svg>
+          </i> </a>
+        </div>
+      </div>
+      <div className={styles.imagem}>
+        <img src={img} alt={title} width="320px" height="320px" />
+      </div>
+    </div>
+  )
+}
--- src/components/Solucoes/index.tsx
+++ src/components/Solucoes/index.tsx
@@ -1,5 +1,10 @@
 import group46 from '../../assets/Group46.svg'
+import group45 from '../../assets/Group45.svg'
+import group48 from '../../assets/Group48.svg'
+import group47 from '../../assets/Group47.svg'
+import { CardImg } from '../CardImg';
 import styles from "./styles.module.scss";
+import { CardImgReverse } from '../CardImgReverse';
 
 
 
@@ -10,19 +15,23 @@ export function Solucoes() {
         <h4>Conheça algumas de nossas soluções</h4>
         <p>A inteligência artificial é altamente versátil podendo ser utilizada em qualquer área</p>
       </div>
-      <div className={styles.subContainer}>
-        <div className={styles.cardContainer}>
-          <div>
-            <img src={group46} />
-
+      <div className={styles.containerCards}>
+        <div>
+          <div className={styles.cards}>
+            <CardImg title="Predição de doenças" img={group46} subTexto="Identifique doenças antes que os sintomas apareçam aumentando a precisão do diagnóstico médico" />
           </div>
-          <div className={styles.card}>
-            <h6>Predição de doenças</h6>
-            <p>Identifique doenças antes que os sintomas apareçam aumentando a precisão do diagnóstico médico</p>
-            <a>Saiba mais <i> - </i> </a>
+          <div className={styles.cards}>
+            <CardImg title="Mineração de dados" img={group48} subTexto="Defina indicadores para avaliar seu negócio e identificar empecilhos de evolução" />
           </div>
         </div>
-
+        <div>
+          <div className={styles.cards}>
+            <CardImgReverse title="Ordem de prioridade" img={group45} subTexto="Automatize seus processos manuais como: despache de materiais, triagem, ordem de prioridade de assistência técnica e diversas outras aplicações" />
+          </div>
+          <div className={styles.cards}>
+            <CardImgReverse title="Sistema de recomendação" img={group47} subTexto="Mecanismos de busca que combinam as preferências do usuário com histórico de dados a fim de indicar a melhor escolha" />
+          </div>
+        </div>
       </div>
 
     </section >

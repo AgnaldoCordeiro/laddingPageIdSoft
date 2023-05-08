d4b70b0fd7fa4052d43e6f5318dd20a363d9de9b
AgnaldoCordeiro <agske13@gmail.com>
2023-05-08 17:30:30 -0300

    :hammer: arrumando bug do build

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,1 +1,20 @@
-    
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Beneficios from './components/Beneficios';
+import Solucoes from './components/Solucoes';
+import Contato from './components/Contato';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Beneficios />} />
+        <Route path="/beneficios" element={<Beneficios />} />
+        <Route path="/solucoes" element={<Solucoes />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
